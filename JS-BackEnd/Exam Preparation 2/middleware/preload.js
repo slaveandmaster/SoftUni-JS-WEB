69437a168f71b8e396ad1c69807aab0dec1ec96e
slaveandmaster <slaveandmaster@gmail.com>
2022-02-23 17:34:39 +0200

    edit preload middleware

--- JS-BackEnd/Exam Preparation 2/middleware/preload.js
+++ JS-BackEnd/Exam Preparation 2/middleware/preload.js
@@ -2,7 +2,7 @@ const tripService = require("../services/trip");
 
 function preload(populate) {
   return async function (req, res, next) {
-    console.log(req.params);
+    
     const id = req.params.id;
     if (populate) {
       res.locals.trip = await tripService.getTripAndUsers(id);
